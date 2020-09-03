97294c8aa1e02634064b3ce6f4d637a4c08e7b94
Anderson Ono <andeerson.ono@gmail.com>
2020-09-02 21:09:27 -0300

    :rocket: Profile

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -22,6 +22,7 @@ import {
 import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 import { parseISO } from 'date-fns/esm';
+import { Link } from 'react-router-dom';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -143,7 +144,10 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
+
             </div>
           </Profile>
 
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -47,15 +47,26 @@ export const Profile = styled.div`
     flex-direction: column;
     margin-left: 16px;
     line-height: 24px;
+
+    span {
+      color: #f4ede8;
+    }
+
+    strong {
+      color: #ff9000;
+    }
+
+    a {
+      text-decoration: none;
+      color: #ff9000;
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
   }
 
-  span {
-    color: #f4ede8;
-  }
 
-  strong {
-    color: #ff9000;
-  }
 `;
 
 export const Content = styled.main`
